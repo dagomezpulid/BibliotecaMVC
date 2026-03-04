d09412a12043661653a6ec54d6b6a7f15fc37f0f
dagomezpulid <dgomezpulid@outlook.com>
2026-03-04 12:24:32 -0500

    commit

--- BibliotecaMVC/Controllers/AdminController.cs
+++ BibliotecaMVC/Controllers/AdminController.cs
@@ -3,39 +3,34 @@ using BibliotecaMVC.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [Authorize(Roles = "Admin")]
 public class AdminController : Controller
 {
+    private readonly BibliotecaContext _context;
     private readonly UserManager<ApplicationUser> _userManager;
 
-    public AdminController(UserManager<ApplicationUser> userManager)
+    public AdminController(BibliotecaContext context, UserManager<ApplicationUser> userManager)
     {
+        _context = context;
         _userManager = userManager;
     }
-
-    public IActionResult Index()
-    {
-        var users = _userManager.Users.ToList();
-        return View(users);
-    }
     //ToggleAdmin
-    public async Task<IActionResult> ToggleAdmin()
+    public async Task<IActionResult> Index()
     {
-        var users = _userManager.Users.ToList();
-
-        var model = new List<UserViewModel>();
-
-        foreach (var user in users)
+        var model = new AdminDashboardViewModel
         {
-            model.Add(new UserViewModel
-            {
-                Id = user.Id,
-                NombreCompleto = user.NombreCompleto,
-                Email = user.Email!,
-                EsAdmin = await _userManager.IsInRoleAsync(user, "Admin")
-            });
-        }
+            TotalUsuarios = _userManager.Users.Count(),
+            TotalLibros = await _context.Libros.CountAsync(),
+            TotalAutores = await _context.Autores.CountAsync(),
+            PrestamosActivos = await _context.Prestamos
+                        .Where(p => p.FechaDevolucion == null)
+                        .CountAsync(),
+            TotalMultasPendientes = await _context.Prestamos
+                        .Where(p => p.Multa > 0)
+                        .SumAsync(p => (decimal?)p.Multa) ?? 0
+        };
 
         return View(model);
     }
---
+++ BibliotecaMVC/Models/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaMVC.Models.ViewModels
+{
+    public class AdminDashboardViewModel
+    {
+        public int TotalUsuarios { get; set; }
+        public int TotalLibros { get; set; }
+        public int TotalAutores { get; set; }
+        public int PrestamosActivos { get; set; }
+        public decimal TotalMultasPendientes { get; set; }
+    }
+}
